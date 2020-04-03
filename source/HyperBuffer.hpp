1ea3263878fdee8fa9aa6b83ed2417f0a2b8015b
Lorenz Bucher <lorenz.bucher@gmail.com>
2020-04-03 21:35:30 +0200

    Basic design done, first test running

--- source/HyperBuffer.hpp
+++ source/HyperBuffer.hpp
@@ -1,10 +1,65 @@
 
-#include <iostream>
+#include <memory>
+#include <vector>
 
+#include "HyperBufferData.hpp"
+#include "TemplateUtils.hpp"
+
+// MARK: - HyperBuffer declaration
+template<typename T, int N>
 class HyperBuffer
 {
+protected:
+    using size_type = int;
+    using data_type = typename add_pointers_to_type<T, N>::type;
+
 public:
-    HyperBuffer() {}
+    /** Construct from dimesions sizes, allocate data */
+    template<typename... I>
+    explicit HyperBuffer(I... i);
+
+    /** Construct from pre-allocated, multi-dimensional data */
+    template<typename... I>
+    explicit HyperBuffer(data_type preAllocatedData, I... i);
     
+    /** Construct from pre-allocated, flat (1D) data */
+    template<typename... I, int V=N, typename std::enable_if<V!=1>::type>
+    explicit HyperBuffer(T* preAllocatedDataFlat, I... i)
+    {
+        // TODO:
+    }
+
+    decltype(auto) operator[] (size_type i) { return m_dataPointers[i]; }
+
+    template<int ...I>
+    decltype(auto) operator() (size_type i...) { return m_dataPointers[i]; }
     
+    data_type data() const { return m_dataPointers; }
+    decltype(auto) at(size_type offset) const;
+    
+    int* dims() const { return m_dataObject->m_dimensions.data(); }
+    int* dim(int i) const { return m_dataObject->m_dimensions[i]; }
+
+private:
+    std::unique_ptr<HyperBufferData<T, N>> m_dataObject;
+    
+    data_type m_dataPointers;
 };
+
+// MARK: - HyperBuffer definiion
+template<typename T, int N>
+template<typename... I>
+HyperBuffer<T, N>::HyperBuffer(I... i)
+{
+    m_dataObject = std::make_unique<HyperBufferDataOwning<T, N>>(i...);
+    m_dataPointers = m_dataObject->getDataPointers();
+}
+
+template<typename T, int N>
+template<typename... I>
+HyperBuffer<T, N>::HyperBuffer(data_type preAllocatedData, I... i)
+{
+    m_dataObject = std::make_unique<HyperBufferDataPreAlloc<T, N>>(preAllocatedData, i...);
+    m_dataPointers = m_dataObject->getDataPointers();
+}
+
--- test/HyperBufferTests.cpp
+++ test/HyperBufferTests.cpp
@@ -1,11 +1,76 @@
 
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 
 #include "HyperBuffer.hpp"
 
 TEST_CASE("HyperBuffer Tests")
 {
-    HyperBuffer buffer;
-    
+    SECTION("Build 1D owning") {
+        HyperBuffer<int, 1> arrayI1(4);
+        REQUIRE(arrayI1.dims()[0] == 4);
+        arrayI1[0] = 0;
+        arrayI1[1] = -1;
+        arrayI1[2] = -2;
+        arrayI1[3] = -3;
+        REQUIRE(arrayI1.data() != nullptr);
+        int* rawData = arrayI1.data();
+        REQUIRE(rawData[0] == 0);
+        REQUIRE(rawData[1] == -1);
+        REQUIRE(rawData[2] == -2);
+        REQUIRE(rawData[3] == -3);
+    }
+    SECTION("Build 2D owning") {
+        HyperBuffer<int, 2> arrayI1(4, 2);
+        REQUIRE(arrayI1.dims()[0] == 4);
+        REQUIRE(arrayI1.dims()[1] == 2);
+        arrayI1[0][0] = 0;
+        arrayI1[0][1] = -1;
+        arrayI1[0][2] = -2;
+        arrayI1[0][3] = -3;
+        arrayI1[1][0] = -10;
+        arrayI1[1][1] = -11;
+        arrayI1[1][2] = -22;
+        arrayI1[1][3] = -33;
+        REQUIRE(arrayI1.data() != nullptr);
+        int** rawData = arrayI1.data();
+        REQUIRE(rawData[0][0] == 0);
+        REQUIRE(rawData[0][1] == -1);
+        REQUIRE(rawData[0][2] == -2);
+        REQUIRE(rawData[0][3] == -3);
+        REQUIRE(rawData[1][0] == -10);
+        REQUIRE(rawData[1][1] == -11);
+        REQUIRE(rawData[1][2] == -22);
+        REQUIRE(rawData[1][3] == -33);
+    }
+
+    //HyperBuffer<float, 3> arrayF3(3, 3);
+
     
 }
+
+TEST_CASE("External allocation")
+{
+    int buffer[] { 1, 2, 3, 4, 5 };
+
+    HyperBuffer<int, 1> array(&buffer[0], 5);   // data is not copied
+    //array(0) = 99;
+//    REQUIRE(buffer[0] == 99);
+//    REQUIRE(array.dims()[0] == 5);
+}
+
+//TEST_CASE("Initializer List Construction")
+//{
+//    MultiDimArray<float, 1>  array1  {{3, 4, 5}};
+//    MultiDimArray<float, 2>  array2 {{3, 4, 5}, {3, 4, 5}};
+//
+//}
+//
+//TEST_CASE("Sub-Array Assignmemt")
+//{
+//    MultiDimArray<float, 1>  array1  {{3, 4, 5}};
+//    MultiDimArray<float, 2>  array2 {{3, 4, 5}, {3, 4, 5}};
+//
+//
+//    MultiDimArray<float, 1> array2sub1 = array2(0);
+//}
+
