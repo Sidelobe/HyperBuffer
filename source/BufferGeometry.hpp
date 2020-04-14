68f8740176be903b5dd59242c449f1b4bf2036a6
Lorenz Bucher <lorenz.bucher@sennheiser.com>
2020-04-14 22:32:30 +0200

    BufferGeometry: pointer offsets working

--- source/BufferGeometry.hpp
+++ source/BufferGeometry.hpp
@@ -7,66 +7,84 @@
 
 #include <memory>
 #include <array>
+#include <iostream>
 
 #include "TemplateUtils.hpp"
 
 template<int N>
-struct BufferGeometry
+class BufferGeometry
 {
     using dim_type = int;
+    
+public:
     template<typename... I>
     explicit BufferGeometry(I... i) : m_dimensions{i...}
     {
         static_assert(sizeof...(I) == N, "Incorrect number of arguments");
     }
-    
+
     /** The data is saved in row-major ordering */
     template<typename... I>
-    int getOffsetForIndex(int dn, I... dk) const
+    int getOffsetInDataArray(int dn, I... dk) const
     {
         static_assert(sizeof...(I) == N-1, "Incorrect number of arguments");
-//        std::cout << "Getting: " << dn << " ";
-//        int values[]{ dk... };
-//        if (sizeof...(dk) > 0){
-//            for (int i=0; i< sizeof...(dk); ++i) {
-//                std::cout << values[i] << " ";
-//            }
-//            std::cout << std::endl;
-//        }
-        // NOTE: start with pos=1 - we don't care about highest dimension's value here
-        return getOffset(1, 0, dn, dk...);
+        return getOffset(1, 0, dn, dk...);  // NOTE: start with dimIndex=1 - don't care about highest dimension's value
     }
     
-    
-    
+    /** DimIdx corresponds to index in dimensions array, i.e. DimIdx=0 is the highest-order dimension */
     template<int DimIdx, typename... I>
     int getOffsetInPointerArray(int dn, I... dk) const
     {
         static_assert(sizeof...(I) == DimIdx, "Number of arguments should be DimIdx+1");
- 
-        if (DimIdx == 0) {
-            return dn;
+        static_assert(DimIdx < N-1, "Can only get pointers for any but the lowest-order dimension");
+
+        int sumOfAllHigherDimPointers = 0;
+        // --> sum of cumulative product "stopped early"
+        for (int i=0; i < DimIdx; ++i) {
+            int p = arrayProduct(m_dimensions, 0, i+1);
+            sumOfAllHigherDimPointers += p;
         }
-         return m_dimensions[0] + getOffset(DimIdx, 0, dn, dk...);
+        
+        // Calcualte offset within DimIdx
+        std::cout << "Getting: <DimIndex=" << DimIdx <<"> " << dn << " ";
+        int values[]{ dk... };
+        if (sizeof...(dk) > 0){
+            for (int i=0; i< sizeof...(dk); ++i) {
+                std::cout << values[i] << " ";
+            }
+        }
+        std::cout << "sumOfAllHigherDimPointers=" << sumOfAllHigherDimPointers;
+        std::cout << " ... getOffsetMinusOne(DimIdx)=" << getOffset<N-1>(1, 0, dn, dk...) << std::endl;
+        return sumOfAllHigherDimPointers + getOffset<N-1>(1, 0, dn, dk...);
     }
+
+    //std::array<int, N> getDimensions() const { return m_dimensions; }
     
-    int getOffsetForDimensionStart(int dimensionIndex) const
+protected:
+    template<int Nmax=N>
+    int getOffset(int dimIndex, int cumulativeOffset, int d0) const
     {
-        // TODO: generic way std::make_index_sequence<N>{} ?
-        if (dimensionIndex == 0) {
-            return getOffset(0, 0, 0);
-        } else if (dimensionIndex == 1) {
-            return getOffset(0, 0, 0, 0);
-        } else if (dimensionIndex == 2) {
-            return getOffset(0, 0, 0, 0, 0);
-        } else if (dimensionIndex == 3) {
-            return getOffset(0, 0, 0, 0, 0);
-        } else {
-            assert(true && "unhandled case");
-            return 0;
-        }
+        return cumulativeOffset + d0; // lowest-order dimension (dimIndex = N)
     }
     
+    template<int Nmax=N, typename... I>
+    int getOffset(int dimIndex, int cumulativeOffset, int dn, I... dk) const
+    {
+        int p = arrayProduct(m_dimensions, dimIndex, Nmax); // Size of a block of data "above" this dimIndex
+        int index = p * dn + cumulativeOffset;
+        return getOffset<Nmax>(dimIndex+1, index, dk...); // recusive call
+    }
+    
+    /** Calculates the product of the elements in range [start, end[ of the supplied array */
+    static int arrayProduct(const int (&array)[N], int start = 0, int end = N)
+    {
+        int product = 1;
+        for (int i = start; i < end; ++i) {
+            product *= array[i];
+        }
+        return product;
+    }
+
     // NOTE: we can choose any pointer type here, since all pointers types are same size
     template<typename T, typename std::enable_if<!std::is_pointer<T>::value>::type* = nullptr>
     void hookupPointerArrayToData(const T* dataArray, T** pointerArray)
@@ -74,32 +92,18 @@ struct BufferGeometry
         // Get
         
         // skip lowest dimension
-//        for (int dimIndex = m_dimensions.size()-1; dimIndex > 0; --dimIndex) {
-//            for (int dimValueIndex = 0; dimValueIndex < m_dimensions[dimIndex]; ++dimValueIndex) {
-//                int dimStart = getOffsetForDimensionStart(dimValueIndex);
-////                std::cout << "dimStart: " << dimStart;
-//                int offset = dimStart + dimValueIndex;
-////                std::cout << " offset: " << offset << std::endl;
-//                pointerArray[offset] = (float*)(dataArray + offset);
-//            }
-//        }
+        //        for (int dimIndex = m_dimensions.size()-1; dimIndex > 0; --dimIndex) {
+        //            for (int dimValueIndex = 0; dimValueIndex < m_dimensions[dimIndex]; ++dimValueIndex) {
+        //                int dimStart = getOffsetForDimensionStart(dimValueIndex);
+        ////                std::cout << "dimStart: " << dimStart;
+        //                int offset = dimStart + dimValueIndex;
+        ////                std::cout << " offset: " << offset << std::endl;
+        //                pointerArray[offset] = (float*)(dataArray + offset);
+        //            }
+        //        }
     }
-    
-    std::array<int, N> getDimensions() const { return m_dimensions; }
     
 private:
-    template<typename... I>
-    int getOffset(int pos, int prevOffset, int d0) const
-    {
-        return prevOffset + d0;
-    }
+    int m_dimensions[N];
     
-    template<typename... I>
-    int getOffset(int pos, int prevOffset, int dn, I... dk) const
-    {
-        int index = m_dimensions[pos] * dn + prevOffset;
-        return getOffset(pos + 1, index, dk...); // recusive call
-    }
-    
-    std::array<int, N> m_dimensions;
 };
--- test/tests/BufferGeometryTests.cpp
+++ test/tests/BufferGeometryTests.cpp
@@ -15,12 +15,13 @@ TEST_CASE("BufferGeometry Tests")
 {
     SECTION("1D") {
         BufferGeometry<1> bufferGeo(3);
-        CHECK(bufferGeo.getOffsetForIndex(0) == 0);
-        CHECK(bufferGeo.getOffsetForIndex(1) == 1);
-        CHECK(bufferGeo.getOffsetForIndex(2) == 2);
-        float data [3] {0};
-        float* pointers [3];
-        bufferGeo.hookupPointerArrayToData(data, pointers);
+        CHECK(bufferGeo.getOffsetInDataArray(0) == 0);
+        CHECK(bufferGeo.getOffsetInDataArray(1) == 1);
+        CHECK(bufferGeo.getOffsetInDataArray(2) == 2);
+       
+//        float data [3] {0};
+//        float* pointers [3];
+//        bufferGeo.hookupPointerArrayToData(data, pointers);
         
 //        CHECK(bufferGeo.getOffsetInPointerArray(0, 0) == 0);
 //        CHECK(bufferGeo.getOffsetInPointerArray(1, 0) == 1);
@@ -29,18 +30,19 @@ TEST_CASE("BufferGeometry Tests")
 //        CHECK(pointers[1] == &data[(bufferGeo.getOffsetForIndex(1))]);
 //        CHECK(pointers[2] == &data[(bufferGeo.getOffsetForIndex(2))]);
     }
+    
     SECTION("2D") {
         BufferGeometry<2> bufferGeo(3, 3);
-        CHECK(bufferGeo.getOffsetForIndex(0, 0) == 0);
-        CHECK(bufferGeo.getOffsetForIndex(0, 1) == 1);
-        CHECK(bufferGeo.getOffsetForIndex(0, 2) == 2);
-        CHECK(bufferGeo.getOffsetForIndex(1, 0) == 3);
-        CHECK(bufferGeo.getOffsetForIndex(1, 1) == 4);
-        CHECK(bufferGeo.getOffsetForIndex(1, 2) == 5);
-        CHECK(bufferGeo.getOffsetForIndex(2, 0) == 6);
-        CHECK(bufferGeo.getOffsetForIndex(2, 1) == 7);
-        CHECK(bufferGeo.getOffsetForIndex(2, 2) == 8);
-        
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0) == 0);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1) == 1);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2) == 2);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 0) == 3);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 1) == 4);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 2) == 5);
+        CHECK(bufferGeo.getOffsetInDataArray(2, 0) == 6);
+        CHECK(bufferGeo.getOffsetInDataArray(2, 1) == 7);
+        CHECK(bufferGeo.getOffsetInDataArray(2, 2) == 8);
+
 //        CHECK(bufferGeo.getOffsetInPointerArray(0, 0) == 0);
 //        CHECK(bufferGeo.getOffsetInPointerArray(1, 0) == 1);
 //        CHECK(bufferGeo.getOffsetInPointerArray(2, 0) == 2);
@@ -53,16 +55,25 @@ TEST_CASE("BufferGeometry Tests")
 //        REQUIRE(pointers[1] == &data[(bufferGeo.getOffsetForIndex(1, 0))]);
 //        REQUIRE(pointers[2] == &data[(bufferGeo.getOffsetForIndex(2, 0))]);
     }
+    
     SECTION("3D") {
-        BufferGeometry<3> bufferGeo(2, 4, 1);
-        CHECK(bufferGeo.getOffsetForIndex(0, 0, 0) == 0);
-        CHECK(bufferGeo.getOffsetForIndex(0, 1, 0) == 1);
-        CHECK(bufferGeo.getOffsetForIndex(0, 2, 0) == 2);
-        CHECK(bufferGeo.getOffsetForIndex(0, 3, 0) == 3);
-        CHECK(bufferGeo.getOffsetForIndex(1, 0, 0) == 4);
-        CHECK(bufferGeo.getOffsetForIndex(1, 1, 0) == 5);
-        CHECK(bufferGeo.getOffsetForIndex(1, 2, 0) == 6);
-        CHECK(bufferGeo.getOffsetForIndex(1, 3, 0) == 7);
+        BufferGeometry<3> bufferGeo(2, 4, 2);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0, 0) == 0);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0, 1) == 1);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1, 0) == 2);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1, 1) == 3);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2, 0) == 4);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2, 1) == 5);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 3, 0) == 6);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 3, 1) == 7);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 0, 0) == 8);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 0, 1) == 9);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 1, 0) == 10);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 1, 1) == 11);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 2, 0) == 12);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 2, 1) == 13);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 3, 0) == 14);
+        CHECK(bufferGeo.getOffsetInDataArray(1, 3, 1) == 15);
         
         CHECK(bufferGeo.getOffsetInPointerArray<0>(0) == 0);
         CHECK(bufferGeo.getOffsetInPointerArray<0>(1) == 1);
@@ -78,32 +89,31 @@ TEST_CASE("BufferGeometry Tests")
     }
     SECTION("4D") {
         BufferGeometry<4> bufferGeo(1, 3, 2, 2);
-        CHECK(bufferGeo.getOffsetForIndex(0, 0, 0, 0) == 0);
-        CHECK(bufferGeo.getOffsetForIndex(0, 0, 0, 1) == 1);
-        CHECK(bufferGeo.getOffsetForIndex(0, 0, 1, 0) == 2);
-        CHECK(bufferGeo.getOffsetForIndex(0, 0, 1, 1) == 3);
-        CHECK(bufferGeo.getOffsetForIndex(0, 1, 0, 0) == 4);
-        CHECK(bufferGeo.getOffsetForIndex(0, 1, 0, 1) == 5);
-        CHECK(bufferGeo.getOffsetForIndex(0, 1, 1, 0) == 6);
-        CHECK(bufferGeo.getOffsetForIndex(0, 1, 1, 1) == 7);
-        CHECK(bufferGeo.getOffsetForIndex(0, 2, 0, 0) == 8);
-        CHECK(bufferGeo.getOffsetForIndex(0, 2, 0, 1) == 9);
-        CHECK(bufferGeo.getOffsetForIndex(0, 2, 1, 0) == 10);
-        CHECK(bufferGeo.getOffsetForIndex(0, 2, 1, 1) == 11);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0, 0, 0) == 0);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0, 0, 1) == 1);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0, 1, 0) == 2);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 0, 1, 1) == 3);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1, 0, 0) == 4);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1, 0, 1) == 5);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1, 1, 0) == 6);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 1, 1, 1) == 7);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2, 0, 0) == 8);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2, 0, 1) == 9);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2, 1, 0) == 10);
+        CHECK(bufferGeo.getOffsetInDataArray(0, 2, 1, 1) == 11);
 
         CHECK(bufferGeo.getOffsetInPointerArray<0>(0) == 0);
 
         CHECK(bufferGeo.getOffsetInPointerArray<1>(0, 0) == 1);
         CHECK(bufferGeo.getOffsetInPointerArray<1>(0, 1) == 2);
         CHECK(bufferGeo.getOffsetInPointerArray<1>(0, 2) == 3);
-        CHECK(bufferGeo.getOffsetInPointerArray<1>(0, 3) == 4);
-        
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 0, 0) == 5);
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 0, 1) == 6);
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 0, 2) == 7);
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 1, 0) == 8);
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 1, 1) == 9);
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 1, 2) == 10);
-        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 1, 3) == 11);
+
+        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 0, 0) == 4);
+        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 0, 1) == 5);
+        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 1, 0) == 6);
+        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 1, 1) == 7);
+        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 2, 0) == 8);
+        CHECK(bufferGeo.getOffsetInPointerArray<2>(0, 2, 1) == 9);
     }
+
 }
