985346c421eb7919fc64a5cd62b3c35308df6a85
Lorenz Bucher <lorenz.bucher@gmail.com>
2020-04-16 19:49:34 +0200

    offset calculations as free functions

--- source/BufferGeometry.hpp
+++ source/BufferGeometry.hpp
@@ -11,6 +11,35 @@
 
 #include "TemplateUtils.hpp"
 
+namespace OffsetCalculation
+{
+/** Calculates the product of the elements in range [start, end[ of the supplied array */
+template<int N>
+static int arrayProduct(int const* array, int start = 0, int end = N)
+{
+    int product = 1;
+    for (int i = start; i < end; ++i) {
+        product *= array[i];
+    }
+    return product;
+}
+
+template<int Nmax>
+int getOffset(int dimIndex, int cumulativeOffset, int const* dimExtents, int d0)
+{
+    return cumulativeOffset + d0; // lowest-order dimension (dimIndex = N)
+}
+
+template<int Nmax, typename... I>
+int getOffset(int dimIndex, int cumulativeOffset, int const* dimExtents, int dn, I... dk)
+{
+    int p = arrayProduct<Nmax>(dimExtents, dimIndex, Nmax); // Size of a block of data "above" this dimIndex
+    int index = p * dn + cumulativeOffset;
+    return getOffset<Nmax>(dimIndex+1, index, dimExtents, dk...); // recusive call
+}
+
+} // namespace OffsetCalculation
+
 template<int N>
 class BufferGeometry
 {
@@ -28,7 +57,7 @@ public:
     int getOffsetInDataArray(int dn, I... dk) const
     {
         static_assert(sizeof...(I) == N-1, "Incorrect number of arguments");
-        return getOffset(1, 0, dn, dk...);  // NOTE: start with dimIndex=1 - don't care about highest dimension's value
+        return OffsetCalculation::getOffset<N>(1, 0, m_dimensions, dn, dk...);  // NOTE: start with dimIndex=1 - don't care about highest dimension's value
     }
     
     /** DimIdx corresponds to index in dimensions array, i.e. DimIdx=0 is the highest-order dimension */
@@ -41,7 +70,7 @@ public:
         int sumOfAllHigherDimPointers = 0;
         // --> sum of cumulative product "stopped early"
         for (int i=0; i < DimIdx; ++i) {
-            int p = arrayProduct(m_dimensions, 0, i+1);
+            int p = OffsetCalculation::arrayProduct<N>(m_dimensions, 0, i+1);
             sumOfAllHigherDimPointers += p;
         }
         
@@ -54,37 +83,13 @@ public:
             }
         }
         std::cout << "sumOfAllHigherDimPointers=" << sumOfAllHigherDimPointers;
-        std::cout << " ... getOffsetMinusOne(DimIdx)=" << getOffset<N-1>(1, 0, dn, dk...) << std::endl;
-        return sumOfAllHigherDimPointers + getOffset<N-1>(1, 0, dn, dk...);
+        std::cout << " ... getOffsetMinusOne(DimIdx)=" << OffsetCalculation::getOffset<N-1>(1, 0, m_dimensions, dn, dk...) << std::endl;
+        return sumOfAllHigherDimPointers + OffsetCalculation::getOffset<N-1>(1, 0, m_dimensions, dn, dk...);
     }
 
     //std::array<int, N> getDimensions() const { return m_dimensions; }
     
 protected:
-    template<int Nmax=N>
-    int getOffset(int dimIndex, int cumulativeOffset, int d0) const
-    {
-        return cumulativeOffset + d0; // lowest-order dimension (dimIndex = N)
-    }
-    
-    template<int Nmax=N, typename... I>
-    int getOffset(int dimIndex, int cumulativeOffset, int dn, I... dk) const
-    {
-        int p = arrayProduct(m_dimensions, dimIndex, Nmax); // Size of a block of data "above" this dimIndex
-        int index = p * dn + cumulativeOffset;
-        return getOffset<Nmax>(dimIndex+1, index, dk...); // recusive call
-    }
-    
-    /** Calculates the product of the elements in range [start, end[ of the supplied array */
-    static int arrayProduct(const int (&array)[N], int start = 0, int end = N)
-    {
-        int product = 1;
-        for (int i = start; i < end; ++i) {
-            product *= array[i];
-        }
-        return product;
-    }
-
     // NOTE: we can choose any pointer type here, since all pointers types are same size
     template<typename T, typename std::enable_if<!std::is_pointer<T>::value>::type* = nullptr>
     void hookupPointerArrayToData(const T* dataArray, T** pointerArray)
