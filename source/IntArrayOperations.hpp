c4656576240153ae420b11ea71a01f592bf7fd8f
Lorenz Bucher <lorenz.bucher@gmail.com>
2020-04-20 00:40:32 +0200

    added std::array operations

--- source/IntArrayOperations.hpp
+++ source/IntArrayOperations.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <array>
+
 #include "TemplateUtils.hpp"
 
 namespace IntArrayOperations
@@ -73,5 +75,67 @@ constexpr int productCapped(int cap, const int (&array)[N])
 
 } // namespace IntArrayOperations
 
+namespace StdArrayOperations
+{
 
+/** Calculates the sum of the elements of the supplied array */
+template<std::size_t N>
+constexpr int sum(const std::array<int, N>& array)
+{
+    return VarArgOperations::apply([](auto&&... args)
+    {
+        return VarArgOperations::sum(std::forward<decltype(args)>(args)...);
+    }, VarArgOperations::makeIntTuple(array));
+}
+
+/** Calculates the sum of the elements in range [start, end[ of the supplied array */
+template<std::size_t N>
+constexpr int sumOverRange(int begin, int end, const std::array<int, N>& array)
+{
+    return VarArgOperations::apply([begin, end](auto&&... args)
+    {
+        return VarArgOperations::sumOverRange(begin, end, std::forward<decltype(args)>(args)...);
+    }, VarArgOperations::makeIntTuple(array));
+}
+/** Calculates the sum of the elements in range [0, cap[ of the supplied array */
+template<std::size_t N>
+constexpr int sumCapped(int cap, const std::array<int, N>& array)
+{
+    return VarArgOperations::apply([cap](auto&&... args)
+    {
+        return VarArgOperations::sumCapped(cap, std::forward<decltype(args)>(args)...);
+    },  VarArgOperations::makeIntTuple(array));
+}
+
+/** Calculates the product of the elements of the supplied array */
+template<std::size_t N>
+constexpr int product(const std::array<int, N>& array)
+{
+    return VarArgOperations::apply([](auto&&... args)
+    {
+        return VarArgOperations::product(std::forward<decltype(args)>(args)...);
+    }, VarArgOperations::makeIntTuple(array));
+}
+
+/** Calculates the product of the elements in range [start, end[ of the supplied array */
+template<std::size_t N>
+constexpr int productOverRange(int begin, int end, const std::array<int, N>& array)
+{
+    return VarArgOperations::apply([begin, end](auto&&... args)
+    {
+        return VarArgOperations::productOverRange(begin, end, std::forward<decltype(args)>(args)...);
+    }, VarArgOperations::makeIntTuple(array));
+}
+
+/** Calculates the product of the elements in range [0, cap[ of the supplied array */
+template<std::size_t N>
+constexpr int productCapped(int cap, const std::array<int, N>& array)
+{
+    return VarArgOperations::apply([cap](auto&&... args)
+    {
+        return VarArgOperations::productCapped(cap, std::forward<decltype(args)>(args)...);
+    }, VarArgOperations::makeIntTuple(array));
+}
+
+} // namespace StdArrayOperations
 
--- test/tests/IntArrayOperationsTests.cpp
+++ test/tests/IntArrayOperationsTests.cpp
@@ -38,3 +38,29 @@ TEST_CASE("IntArrayOperations Tests")
     REQUIRE(productOverRange(0, 4, {3, 1, 2, 5}) == 30);
     REQUIRE(productOverRange(0, 3, {3, 1, 2, 5}) == 6);
 }
+
+TEST_CASE("StdArrayOperations Tests")
+{
+    // NOTE: the 'guts' of these functions are all calculated by VarArg functions, which are more thoroughly tested.
+    // this just verifies parameters are passed correctly.
+    using namespace StdArrayOperations;
+    
+    REQUIRE(sum(std::array<int, 4>{3, 1, 2, 5}) == 11);
+    REQUIRE(sumCapped(1, std::array<int, 2>{3, 2}) == 3);
+    REQUIRE(sumCapped(2, std::array<int, 2>{2, 7}) == 9);
+    REQUIRE(sumCapped(2, std::array<int, 3>{1, 2, 8}) == 3);
+
+    REQUIRE(sumOverRange(1, 4, std::array<int, 4>{3, 1, 2, 5}) == 8);
+    REQUIRE(sumOverRange(0, 4, std::array<int, 4>{3, 1, 2, 5}) == 11);
+    REQUIRE(sumOverRange(0, 3, std::array<int, 4>{3, 1, 4, 5}) == 8);
+   
+    REQUIRE(product(std::array<int, 2>{2, 3}) == 6);
+    REQUIRE(productCapped(2, std::array<int, 3>{1, 2, 0}) == 2);
+    REQUIRE(productCapped(1, std::array<int, 2>{2, 2}) == 2);
+    REQUIRE(productCapped(2, std::array<int, 2>{2, 7}) == 14);
+    REQUIRE(productCapped(2, std::array<int, 3>{1, 2, 0}) == 2);
+
+    REQUIRE(productOverRange(1, 4, std::array<int, 4>{3, 1, 2, 5}) == 10);
+    REQUIRE(productOverRange(0, 4, std::array<int, 4>{3, 1, 2, 5}) == 30);
+    REQUIRE(productOverRange(0, 3, std::array<int, 4>{3, 1, 2, 5}) == 6);
+}
