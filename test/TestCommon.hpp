f739d82d176ef0730dfd93ac13f7cd4b1afeec2d
Lorenz Bucher <lorenz.bucher@gmail.com>
2020-04-27 16:49:32 +0200

    gcc diagnostic

--- test/TestCommon.hpp
+++ test/TestCommon.hpp
@@ -10,21 +10,22 @@
 #include <catch2/catch.hpp>
 
 // Turn off some warnings in tests -- Otherwise some things just become very annoying
+// Clang accepts gcc diagnostic, not the other way around
 #if defined(__clang__) || defined(__GNUC__)
-    #define WARNINGS_OVERRIDE _Pragma("clang diagnostic push")
-    #define WARNINGS_RESTORE_ALL _Pragma("clang diagnostic pop")
+    #define WARNINGS_OVERRIDE _Pragma("gcc diagnostic push")
+    #define WARNINGS_RESTORE_ALL _Pragma("gcc diagnostic pop")
     #define  WARNINGS_DISABLE_SIGN_UNSIGNED \
-        _Pragma("clang diagnostic ignored \"-Wconversion\"") \
-        _Pragma("clang diagnostic ignored \"-Wsign-conversion\"") \
-        _Pragma("clang diagnostic ignored \"-Wsign-compare\"")
-    #define WARNINGS_DISABLE_C_CAST _Pragma("clang diagnostic ignored \"-Wold-style-cast\"")
-    #define WARNINGS_DISABLE_CAST_ALIGN _Pragma("clang diagnostic ignored \"-Wcast-align\"")
+        _Pragma("gcc diagnostic ignored \"-Wconversion\"") \
+        _Pragma("gcc diagnostic ignored \"-Wsign-conversion\"") \
+        _Pragma("gcc diagnostic ignored \"-Wsign-compare\"")
+    #define WARNINGS_DISABLE_C_CAST _Pragma("gcc diagnostic ignored \"-Wold-style-cast\"")
+    #define WARNINGS_DISABLE_CAST_ALIGN _Pragma("gcc diagnostic ignored \"-Wcast-align\"")
 
 #elif defined(_MSC_VER)
     #define WARNINGS_OVERRIDE __pragma( warning(push) )
     #define WARNINGS_RESTORE_ALL __pragma( warning(pop) )
     #define WARNINGS_DISABLE_SIGN_UNSIGNED __pragma(warning( disable : 4245) )
-    #define WARNINGS_DISABLE_C_CAST 
+    #define WARNINGS_DISABLE_C_CAST
     #define WARNINGS_DISABLE_CAST_ALIGN
 
 #endif
