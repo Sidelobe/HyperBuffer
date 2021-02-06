a2f4ad37c213addae0cfd557af1875d1833cb261
Lorenz Bucher <lorenz.bucher@gmail.com>
2021-02-06 17:43:36 +0100

    Removed dead code (all unused wrappers)

--- source/IntArrayOperations.hpp
+++ source/IntArrayOperations.hpp
@@ -21,125 +21,9 @@
 namespace slb
 {
 
-// MARK: - C-Style Arrays
-namespace IntArrayOperations
-{
-
-/** @see CompiletimeMath::sum */
-template<std::size_t N>
-constexpr int sum(const int (&array)[N])
-{
-    return VarArgOperations::apply([](auto&&... args)
-    {
-        return CompiletimeMath::sum(std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumOverRange */
-template<std::size_t N>
-constexpr int sumOverRange(int first, int num, const int (&array)[N])
-{
-    return VarArgOperations::apply([&](auto&&... args)
-    {
-        return CompiletimeMath::sumOverRange(first, num, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumCapped */
-template<std::size_t N>
-constexpr int sumCapped(int cap, const int (&array)[N])
-{
-    return VarArgOperations::apply([&](auto&&... args)
-    {
-        return CompiletimeMath::sumCapped(cap, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::product */
-template<std::size_t N>
-constexpr int product(const int (&array)[N])
-{
-    return VarArgOperations::apply([](auto&&... args)
-    {
-        return CompiletimeMath::product(std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::productOverRange */
-template<std::size_t N>
-constexpr int productOverRange(int first, int num, const int (&array)[N])
-{
-    return VarArgOperations::apply([&](auto&&... args)
-    {
-        return CompiletimeMath::productOverRange(first, num, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::productCapped */
-template<std::size_t N>
-constexpr int productCapped(int cap, const int (&array)[N])
-{
-    return VarArgOperations::apply([cap](auto&&... args)
-    {
-        return CompiletimeMath::productCapped(cap, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumOfCumulativeProduct */
-template<std::size_t N>
-constexpr int sumOfCumulativeProduct(const int (&array)[N])
-{
-    return VarArgOperations::apply([](auto&&... args)
-    {
-        return CompiletimeMath::sumOfCumulativeProduct(std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumOfCumulativeProductCapped */
-template<std::size_t N>
-constexpr int sumOfCumulativeProductCapped(int cap, const int (&array)[N])
-{
-    return VarArgOperations::apply([cap](auto&&... args)
-    {
-        return CompiletimeMath::sumOfCumulativeProductCapped(cap, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-} // namespace IntArrayOperations
-
 // MARK: - std::array
 namespace StdArrayOperations
 {
-
-/** @see CompiletimeMath::sum */
-template<std::size_t N>
-constexpr int sum(const std::array<int, N>& array)
-{
-    return VarArgOperations::apply([](auto&&... args)
-    {
-        return CompiletimeMath::sum(std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumOverRange */
-template<std::size_t N>
-constexpr int sumOverRange(int first, int num, const std::array<int, N>& array)
-{
-    return VarArgOperations::apply([&](auto&&... args)
-    {
-        return CompiletimeMath::sumOverRange(first, num, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumCapped */
-template<std::size_t N>
-constexpr int sumCapped(int cap, const std::array<int, N>& array)
-{
-    return VarArgOperations::apply([&](auto&&... args)
-    {
-        return CompiletimeMath::sumCapped(cap, std::forward<decltype(args)>(args)...);
-    }, array);
-}
 
 /** @see CompiletimeMath::product */
 template<std::size_t N>
@@ -151,16 +35,6 @@ constexpr int product(const std::array<int, N>& array)
     }, array);
 }
 
-/** @see CompiletimeMath::productOverRange */
-template<std::size_t N>
-constexpr int productOverRange(int first, int num, const std::array<int, N>& array)
-{
-    return VarArgOperations::apply([&](auto&&... args)
-    {
-        return CompiletimeMath::productOverRange(first, num, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
 /** @see CompiletimeMath::productCapped */
 template<std::size_t N>
 constexpr int productCapped(int cap, const std::array<int, N>& array)
@@ -168,16 +42,6 @@ constexpr int productCapped(int cap, const std::array<int, N>& array)
     return VarArgOperations::apply([cap](auto&&... args)
     {
         return CompiletimeMath::productCapped(cap, std::forward<decltype(args)>(args)...);
-    }, array);
-}
-
-/** @see CompiletimeMath::sumOfCumulativeProduct */
-template<std::size_t N>
-constexpr int sumOfCumulativeProduct(const std::array<int, N>& array)
-{
-    return VarArgOperations::apply([](auto&&... args)
-    {
-        return CompiletimeMath::sumOfCumulativeProduct(std::forward<decltype(args)>(args)...);
     }, array);
 }
 
@@ -193,7 +57,7 @@ constexpr int sumOfCumulativeProductCapped(int cap, const std::array<int, N>& ar
 
 /** @returns the N-1 sub-array : removes / "shaves off" the first element */
 template<std::size_t N>
-static constexpr std::array<int, N-1> shaveOffFirstElement(const std::array<int, N>& array)
+constexpr std::array<int, N-1> shaveOffFirstElement(const std::array<int, N>& array)
 {
     std::array<int, N-1> subarray;
     for (int i=0; i < N-1; ++i) {
@@ -204,5 +68,4 @@ static constexpr std::array<int, N-1> shaveOffFirstElement(const std::array<int,
 
 
 } // namespace StdArrayOperations
-
 } // namespace slb
