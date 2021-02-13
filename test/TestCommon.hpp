9c7ebf1d4c16e4d3a8f6416c1cd08bb2c5d07eaa
Lorenz Bucher <lorenz.bucher@gmail.com>
2021-02-13 14:11:20 +0100

    Allow compilation without exceptions

--- test/TestCommon.hpp
+++ test/TestCommon.hpp
@@ -9,6 +9,22 @@
 
 #include <catch2/catch.hpp>
 
+/* Macro to detect if exceptions are disabled (works on GCC, Clang and MSVC) 3 */
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+#if !__has_feature(cxx_exceptions) && !defined(__cpp_exceptions) && !defined(__EXCEPTIONS) && !defined(_CPPUNWIND)
+  #define EXCEPTIONS_DISABLED
+#endif
+
+// When exceptions are disbled, we redefine catch2's REQUIRE_THROWS, so we can compile.
+// Any REQUIRE_THROWS statements in tests will dissappear / do nothing
+#if __EXCEPTIONS == 0
+    #define REQUIRE_THROWS_CATCH2 REQUIRE_THROWS
+    #undef REQUIRE_THROWS
+    #define REQUIRE_THROWS(...)
+#endif
+
 namespace TestCommon
 {
 using stl_size_type = typename std::vector<float>::size_type;
