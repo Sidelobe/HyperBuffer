ed4372dbae33d0c154c41d6b4f4dccc0dafb2625
Lorenz Bucher <lorenz.bucher@gmail.com>
2023-04-02 22:39:59 +0200

    updated generator script

--- deploy/generate_single_header_include.sh
+++ deploy/generate_single_header_include.sh
@@ -1,3 +1,3 @@
 cd -- "$(dirname "$BASH_SOURCE")"
 
-python3 amalgamateHeaderFiles.py ../source/HyperBuffer.hpp ../single_include/HyperBuffer.hpp
+python3 ../test/external-utils/HppAmalgamator/amalgamate_header_files.py ../source/HyperBuffer.hpp ../single_include/HyperBuffer.hpp
